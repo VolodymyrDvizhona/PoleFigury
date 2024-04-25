47526dc6f146c35c7a9a1b5c2336da6e0e59c34d
VolodymyrDvizhona <mlunch12@gmail.com>
2024-04-25 10:24:59 +0200

    Add project files.

---
+++ Biblioteka/Pole.cs
@@ -0,0 +1,16 @@
+﻿namespace Biblioteka
+{
+    public class Pole
+    {
+        public static double Kwadrat(double a)
+        {
+            return a * a;
+        }
+
+        public static double Kolo(double r)
+        {
+            return Math.PI * r * r;
+        }
+    }
+
+}
---
+++ TestBiblioteki/Program.cs
@@ -0,0 +1,24 @@
+﻿using Biblioteka;
+namespace TestBiblioteki
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj długość boku do kwadratu: ");
+            double bok =
+                Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Pole kwadratu wynosi {0}.",
+                Pole.Kwadrat(bok));
+
+            Console.WriteLine("\nPodaj długość promienia: ");
+            double promien =
+                Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Pole koła wynosi {0}. ",
+                Pole.Kolo(promien));
+
+            Console.WriteLine("\nKoniec Programu");
+            Console.ReadKey();
+        }
+    }
+}
